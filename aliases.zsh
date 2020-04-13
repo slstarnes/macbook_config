73ad7aec4cc4fae87bb294ea5c1e3d6554577a4d
Luke Starnes <lukestarnes@gmail.com>
2020-04-13 09:56:16 -0400

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,28 @@
+# Aliases
+# alias alias_name="command_to_run"
+
+# Source Reload
+alias reload.zsh="source ~/.zshrc"
+alias reload.profile="source ~/.bash_profile"
+alias reload.term="source ~/.bash_profile && source ~/.zshrc"
+
+# Long format list
+alias ll="ls -lhF"
+alias la="ls -lhAF"
+alias lal="ls -lhAF | less"
+
+# Print my public IP
+alias myip='curl ipinfo.io/ip'
+
+# Grep command line history
+alias gh='history|grep'
+
+# Shorten cd
+alias cd..="cd .."
+alias cd...="cd ../.."
+alias cd....="cd ../../.."
+alias cd.....="cd ../../../.."
+alias cd......="cd ../../../../.."
+
+# History
+alias h='history'
